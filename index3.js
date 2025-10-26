d7a1865e91d1461aa849c2fc31188419ba349c95
sachinsoumya <pandasoumyasachin@gmail.com>
2025-10-26 13:21:58 +0530

    added compound indexes.

--- index3.js
+++ index3.js
@@ -9,24 +9,32 @@ mongoose
 
 const StudentSchema = new mongoose.Schema({
   name: String,
+  stream:String,
+  email: String,
   age: { type: Number, index: true },
   grade: String,
   marks: { type: Number },
+  std: Number,
+  section: String,
 });
 
 //Creating the model
 const Student = mongoose.model("Student", StudentSchema);
 
 StudentSchema.index({ marks: 1 }); //* here also creating index on marrks field mannually
+StudentSchema.index({stream:1 , std:1}); //* here we are creating compound index.(i.e adding index more than one fields)
 
 //creating the document
 
-const s1 = new Student({
-  name: "Sachin",
-  age: 26,
-  grade: "o",
-  marks: 590,
-});
+// const s1 = new Student({
+//   name: "Sachin",
+//   email:"sachin@gmail.com",
+//   age: 26,
+//   grade: "o",
+//   marks: 590,
+//   std:9,
+//   sec:"A",
+// });
 
 // s1.save()
 //   .then((data) => console.log(data))
@@ -62,13 +70,70 @@ const s1 = new Student({
 //   .then((data) => console.log(data))
 //   .catch((err => console.log(err)));
 
-// Student.insertMany([
-//   { name: "Sachin", age: 27, grade: "o", marks: 600 },
-//   { name: "Simi", age: 22, grade: "A", marks: 400 },
-//   { name: "Sonu", age: 30, grade: "B", marks: 550 },
-// ])
-//   .then((data) => console.log("students data saved successfully", data))
-//   .catch((err) => console.log(err));
+Student.insertMany([
+  {
+    name: "Sachin",
+    stream:"Science",
+    email: "sachin@gmail.com",
+    age: 27,
+    grade: "o",
+    marks: 600,
+    std: "10",
+    section: "B",
+  },
+  {
+    name: "Simi",
+     stream:"Arts",
+    email: "simi@gmail.com",
+    age: 22,
+    grade: "A",
+    marks: 400,
+    std: 7,
+    section: "A",
+  },
+  {
+    name: "Sonu",
+     stream:"Commerce",
+    email: "sonu@gmail.com",
+    age: 30,
+    grade: "B",
+    marks: 550,
+    std: 8,
+    section: "C",
+  },
+  {
+    name: "Soumya",
+     stream:"Arts",
+    email: "soumya@gmail.com",
+    age: 37,
+    grade: "a",
+    marks: 530,
+    std: 6,
+    section: "C",
+  },
+  {
+    name: "Sushree",
+     stream:"Science",
+    email: "sushree@gmail.com",
+    age: 28,
+    grade: "b",
+    marks: 510,
+    std: 5,
+    section: "B",
+  },
+  {
+    name: "Dhuluku",
+     stream:"Science",
+    email: "dhuluku@gmail.com",
+    age: 30,
+    grade: "c",
+    marks: 570,
+    std: 8,
+    section: "A",
+  },
+])
+  .then((data) => console.log("students data saved successfully", data))
+  .catch((err) => console.log(err));
 
 (async function getStudents() {
   try {
@@ -76,18 +141,29 @@ const s1 = new Student({
     // console.log("Filtered students are" ,filtered_student);
     // console.log( await Student.collection.getIndexes());
 
-    const filtered_student_mark = await Student.find({ marks: 400 });
+    // const filtered_student_mark = await Student.find({ marks: 400 });
 
-    console.log("filtered students based on marks", filtered_student_mark);
-    const checkIndexing_1 =  await Student.find({ marks: 400 }).explain("executionStats"); // before creatinf index  - COLLSCAN
+    // console.log("filtered students based on marks", filtered_student_mark);
+    // const checkIndexing_1 = await Student.find({ marks: 400 }).explain(
+    //   "executionStats"
+    // ); // before creatinf index  - COLLSCAN
 
-    console.log("Check Indexing in marks" , checkIndexing_1.executionStats.executionStages);
+    // console.log(
+    //   "Check Indexing in marks",
+    //   checkIndexing_1.executionStats.executionStages
+    // );
 
-    const checkIndexing_2 = await Student.find({ age: 30 }).explain(
-      "executionStats"
-    ); // after creatinf index - IXSCAN
+    // const checkIndexing_2 = await Student.find({ age: 30 }).explain(
+    //   "executionStats"
+    // ); // after creatinf index - IXSCAN
 
-    console.log("Check Indexing in age", checkIndexing_2.executionStats.executionStages);
+    // console.log(
+    //   "Check Indexing in age",
+    //   checkIndexing_2.executionStats.executionStages
+    // );
+
+    const checkCompoundIndexing= await Student.find({stream:"Science", std:{$eq:10}}).explain("executionStats");
+    console.log(checkCompoundIndexing.executionStats.executionStages);
   } catch (err) {
     console.log(err);
   }
